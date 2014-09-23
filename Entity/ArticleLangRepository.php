902f37f63182f934e34aa2a0c930ce20ad807689
BabarMajes <al@neomajes.com>
2014-09-23 16:13:56 +0200

    Fixin
    Signed-off-by: BabarMajes <al@neomajes.com>

--- Entity/ArticleLangRepository.php
+++ Entity/ArticleLangRepository.php
@@ -48,4 +48,26 @@ class ArticleLangRepository extends EntityRepository {
         return $query->getResult();
     }
 
+    /**
+     * GET last 5 updated pages
+     */
+    public function searchArticles($lang, $slug, $isActive, $deleted) {
+
+        $currentYear = new \DateTime();
+        $currentYear->setDate(date('Y'), 1, 1)->setTime(0,0,0);
+        
+        $query = $this->createQueryBuilder('a')
+            ->where('a.isActive = :isActive')
+            ->andWhere('a.deleted = :deleted')
+            ->andWhere('a.content LIKE :slug')
+            ->andWhere('a.title LIKE :slug')
+            ->setParameter('isActive', $isActive)
+            ->setParameter('deleted', $deleted)
+            ->setParameter('slug', '%'.$slug.'%')
+            ->orderBy('a.date', 'DESC')
+            ->getQuery();
+
+        return $query->getResult();
+    }
+
 }
